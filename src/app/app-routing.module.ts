8c22b86dc7f044d22e6103e51056dec977027c1c
Dimitrije <dimitrijezdravkovic20@gmail.com>
2023-11-25 21:44:33 +0100

    Dimitrije

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,15 +7,24 @@ import { VezbanjeComponent } from './vezbanje/vezbanje.component';
 import { ResursiComponent } from './resursi/resursi.component';
 import { ONamaComponent } from './o-nama/o-nama.component';
 import { LoginComponent } from './login/login.component';
+import { WebsiteComponent } from './website/website.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'blog', component: BlogComponent},
-  {path: 'forum', component: ForumComponent},
-  {path: 'vezbanje', component: VezbanjeComponent},
-  {path: 'resursi', component: ResursiComponent},
-  {path: 'o nama', component: ONamaComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {
+    path: '',
+    component: WebsiteComponent,
+    children: [
+      {path: '', component: HomeComponent},
+      {path: 'blog', component: BlogComponent},
+      {path: 'forum', component: ForumComponent},
+      {path: 'vezbanje', component: VezbanjeComponent},
+      {path: 'resursi', component: ResursiComponent},
+      {path: 'o nama', component: ONamaComponent},
+      {path: 'login', component: LoginComponent}
+    ]
+  },
+  {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ONamaComponent } from './o-nama/o-nama.component';
 import { LoginComponent } from './login/login.component';
 import { BlogPostComponent } from './blog-post/blog-post.component';
 import { BlogPostPageComponent } from './blog-post-page/blog-post-page.component';
+import { WebsiteComponent } from './website/website.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { BlogPostPageComponent } from './blog-post-page/blog-post-page.component
     ONamaComponent,
     LoginComponent,
     BlogPostComponent,
-    BlogPostPageComponent
+    BlogPostPageComponent,
+    WebsiteComponent
   ],
   imports: [
     BrowserModule,
