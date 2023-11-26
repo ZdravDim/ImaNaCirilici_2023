ea613570ba6b627779ca5e98610192f020a71eb0
Dimitrije <dimitrijezdravkovic20@gmail.com>
2023-11-26 12:12:34 +0100

    login register back button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  logInButtonVisible = true;
 
+  ngOnOnit() {
+    if (localStorage.getItem('')) { //pokupi token iz storage
+      if (false) this.logInButtonVisible = false; //pozovi api i vidi jel validan token
+      else this.logInButtonVisible = true;
+    }
+    else this.logInButtonVisible = true;
+  }
 }
