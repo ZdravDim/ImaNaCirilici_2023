bda6d1c7d8844c688553b313c2fc6f9fb06f4373
Jovan Stankovic <j.stankovic001@gmail.com>
2023-11-26 19:29:30 +0100

    download

--- src/app/resursi/resursi.component.ts
+++ src/app/resursi/resursi.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./resursi.component.css']
 })
 export class ResursiComponent {
+  downloadZip(): void {
+    const filePath = "serbian-language-tools-master.zip";
+    const url = `http://localhost:8080/api/zip/downloadZip/${filePath}`;
 
+
+    fetch(url)
+        .then(response => {
+          if (!response.ok) {
+            console.log("error");
+          }
+          return response.blob();
+        })
+        .then(blob => {
+          const a = document.createElement('a');
+          const url = window.URL.createObjectURL(blob);
+
+          a.href = url;
+          a.download = filePath;
+
+          document.body.appendChild(a);
+          a.click();
+
+          document.body.removeChild(a);
+        })
+        .catch(error => console.error('Грешка приликом преузиманја ЗИП фајла:', error));
+  }
 }
