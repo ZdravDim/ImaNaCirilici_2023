b3943068f25c97a60da23a987c31b8fcc8f4ac4b
OnlySupCall <lukarapaic007@gmail.com>
2023-11-26 22:09:15 +0100

    forum post design

--- src/app/forum/forum.component.ts
+++ src/app/forum/forum.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./forum.component.css']
 })
 export class ForumComponent {
+  constructor() {}
+  comments: HTMLElement | null = null;
+  showCommentsBtn: HTMLElement | null = null;
+  commentsAreShown: boolean = true;
 
+  showComments() {
+    this.comments = document.getElementById("Comments");
+    this.showCommentsBtn = document.getElementById("showComments");
+    this.comments?.classList.toggle("hidden");
+    if (this.commentsAreShown) {
+      if (this.showCommentsBtn) this.showCommentsBtn.innerHTML = "Сакриј коментаре";
+    } else {
+      if (this.showCommentsBtn) this.showCommentsBtn.innerHTML = "Прикажи коментаре";
+    }
+    this.commentsAreShown = !this.commentsAreShown;
+  }
 }
